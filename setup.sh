5f76afb017829e53c4dee68a667653699fe1e4c6
Annus Zulfiqar <annusmanarchitect@gmail.com>
2025-03-21 21:40:42 +0000

    updates

--- setup.sh
+++ setup.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
+# Detect OS type
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    OS_TYPE="mac"
+    SED_COMMAND="sed -i ''"
+    INSTALL_COMMAND="brew install"
+else
+    # Linux and others
+    OS_TYPE="linux"
+    SED_COMMAND="sed -i"
+    INSTALL_COMMAND="sudo apt install"
+fi
+
 # Install zsh
-sudo apt install zsh -y
+if [[ "$OS_TYPE" == "mac" ]]; then
+    brew install zsh
+else
+    sudo apt install zsh -y
+fi
 
 # Show version
 zsh --version
 
 # Set zsh as default shell
-chsh -s /usr/bin/zsh 
+chsh -s $(which zsh)
 
-# Echo zsh. 
+# Echo zsh
 echo $SHELL
 
 # Logout & Login if don't see zsh as default shell.
@@ -22,24 +39,41 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # Use agnoster theme
 # https://github.com/ohmyzsh/ohmyzsh/wiki/Themes#agnoster
 # Edit ZSH_THEME
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
+if [[ "$OS_TYPE" == "mac" ]]; then
+    sed -i '' 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
+else
+    sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
+fi
 
 # Install powerline font
-sudo apt-get install fonts-powerline -y
+if [[ "$OS_TYPE" == "mac" ]]; then
+    brew tap homebrew/cask-fonts
+    brew install --cask font-powerline-symbols
+else
+    sudo apt-get install fonts-powerline -y
+fi
 
 # Install powerlevel10k
-sudo rm -R ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sudo rm -R ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k 2>/dev/null || true
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Edit zshrc and add powerlevel
-sed -i 's/ZSH_THEME="agnoster"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
+if [[ "$OS_TYPE" == "mac" ]]; then
+    sed -i '' 's/ZSH_THEME="agnoster"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
+else
+    sed -i 's/ZSH_THEME="agnoster"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
+fi
 
 # Install plugins (zsh-autosuggestions and zsh-syntax-highlighting)
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions 2>/dev/null || true
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting 2>/dev/null || true
 
 # Enable plugins
-sed  -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
+if [[ "$OS_TYPE" == "mac" ]]; then
+    sed -i '' 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
+else
+    sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
+fi
 
 echo "Successfully Installed. Start new terminal and configure powerlevel10k."
 echo "If p10k configuration wizard does not start automatically, run following"
